a0e6b5f77cbc394367eefbc93cdc7d954851a951
krismac <email@krismac.com>
2018-10-18 07:57:59 +0100

    completed up to MVP 2

---
+++ models/traveller.js
@@ -0,0 +1,35 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+   return this.journeys.filter(journey => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance >= minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => total + journey.distance, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const allModes = this.getModesOfTransport();
+  const ModesArray = [];
+  allModes.forEach(function)
+   if ModesArray //includes
+   uniqueModes.push()
+  };
+};
+
+
+module.exports = Traveller;
